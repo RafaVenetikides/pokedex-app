c5130775d18c51f62a51684dba5ad819d46fcba5
rafaelvenetikides <rafavenetikides@gmail.com>
2025-07-15 22:47:18 -0300

    Fix warning due to incorrect constraint

--- pokedex-app/View/PokemonCellView.swift
+++ pokedex-app/View/PokemonCellView.swift
@@ -62,7 +62,7 @@ class PokemonCellView: UIView {
         NSLayoutConstraint.activate([
             containerView.topAnchor.constraint(equalTo: topAnchor, constant: 4),
             containerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            containerView.trailingAnchor.constraint(equalToSystemSpacingAfter: trailingAnchor, multiplier: -16),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             containerView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -4),
             
             pokemonImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
